e20406231c278a874be8b5e79b3a7b1b2a0a5261
Anuj8826 <singhanuj115@gmail.com>
2016-03-21 23:13:06 +0530

    control statement in templates

--- tut1/views.py
+++ tut1/views.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from tut1 import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    user = {'nickname':'ashu'}
+    return render_template('index.html',
+    	title = 'home',
+    	user = user)
